329153580ce39ecffd3580dbe2681b178706cffc
Sujitha2003 <98878126+Sujitha2003@users.noreply.github.com>
2022-06-10 15:04:34 +0530

    Add files via upload

---
+++ Errlibpackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+
+
+setuptools.setup(
+    name='errorcalc',
+    version='0.0.1',
+    author='Sujithasuresh',
+    author_email='sujithasuresh0307@gmail.com',
+    description='This is a simple package to find error values(Mean absolute error,Mean squared error,Root mean squared error)',
+    packages=["errorcalc"],
+    classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+    ]
+    )
